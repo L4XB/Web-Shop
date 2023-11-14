327cca4e2f9c0250bd81b681c2a260a848326084
Lukas Buck <Lukas.buck@e-mail.de>
2023-11-14 19:24:16 +0100

    SQL Table Schema ✅

---
+++ schema.sql
@@ -0,0 +1,23 @@
+//ProductTable
+
+CREATE TABLE products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    category VARCHAR(50),
+    stock_quantity INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    product_image VARCHAR(300),
+);
+
+
+//UserTable
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    passwort DECIMAL(10, 2) NOT NULL,
+    vorname TEXT,
+    nachname VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
