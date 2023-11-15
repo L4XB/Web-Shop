42585e5be0487fbe5494dbe9a2efd4824caf60ea
QW_ellCODe <walzquentin1@gmail.com>
2023-11-15 17:53:55 +0100

    Remove unnecessary navigation menu from klettergerüst view

--- views/klettergerüst.php
+++ views/klettergerüst.php
@@ -22,15 +22,6 @@
                         <use xlink:href="#bootstrap"></use>
                     </svg>
                 </a>
-                <!---
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-                </ul>
-                -->
                 <li>
                     <a href="#" class="nav-link text-secondary">
                         <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
