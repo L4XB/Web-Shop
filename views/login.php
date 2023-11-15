71581446a6099e5ed191dc7f6e23701a59705147
Lukas Buck <Lukas.buck@e-mail.de>
2023-11-15 12:05:17 +0100

    cleanUp

--- views/login.php
+++ views/login.php
@@ -7,11 +7,6 @@
     <title>Login System</title>
     <link rel="stylesheet" href="../style/style.css">
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -26,7 +21,7 @@
             <button type="submit">Login</button>
         </form>
 
-        <p id="signUp"><a href="signUp.html">SignUp</a></p>
+        <p id="signUp"><a href="signUp.php">SignUp</a></p>
 
     </div>
 
