9c36bb532696f236e258f122f58c98cdb655ef2e
QW_ellCODe <walzquentin1@gmail.com>
2023-11-15 17:40:26 +0100

    Add stylesheet for Klettergerüst page and update navigation menu

--- views/klettergerüst.php
+++ views/klettergerüst.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klettergerüst</title>
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
+    <link rel="stylesheet" href="../style/klettergerüst.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
@@ -19,7 +20,7 @@
                         <use xlink:href="#bootstrap"></use>
                     </svg>
                 </a>
-
+                <!---
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
@@ -27,11 +28,40 @@
                     <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">About</a></li>
                 </ul>
+                -->
+                <li>
+                    <a href="#" class="nav-link text-secondary">
+                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
+                        Home
+                    </a>
+                    </li>
 
                 <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
                     <input type="search" class="form-control form-control-dark" placeholder="Search..."
-                        aria-label="Search">
+                        aria-label="Search" id="s1">
                 </form>
+
+                <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+
+                    <li>
+                    <a href="#" class="nav-link text-white">
+                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#table"></use></svg>
+                        Artikelübersicht
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#" class="nav-link text-white">
+                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#grid"></use></svg>
+                        Warenkorb
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#" class="nav-link text-white">
+                        <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"></use></svg>
+                        Profil
+                    </a>
+                    </li>
+                </ul>
 
                 <div class="text-end">
                     <button type="button" class="btn btn-outline-light me-2">Login</button>
@@ -45,3 +75,5 @@
 </body>
 
 </html>
+
+
