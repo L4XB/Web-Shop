5d11cc4353996596140297df4e38cfacebcc732b
QW_ellCODe <walzquentin1@gmail.com>
2023-11-16 22:39:48 +0100

    Fix header and footer styling

--- views/klettergerüst.php
+++ views/klettergerüst.php
@@ -14,18 +14,46 @@
 </head>
 
 <body>
-    <header>
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+            <div class="row">
+                <!-- Logo -->
+                <div class="col-md-2">
+                  <div>
+                    <a href="startseite.php"><img style="width:169px; hight:87px" src="../assets/images/fsi_logo_header.png" alt="logo" class="logo"></a>
+                  </div>
+                </div>
+                <!-- Searchbar -->
+                <div class="col-md-8">
+                  <form>
+                    <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                  </form>
+                </div>
+                <!-- Buttons -->
+                <div class="col-md-2">
+                  <ul class="d-flex list-unstyled">
+                    <li class="me-2"><a href="products.php" class="nav-link px-2 text-white">Artikelübersicht</a></li>
+                    <li class="me-2"><a href="favorits.php" class="nav-link px-2 text-white">Favoriten</a></li>
+                    <li class="me-2"><a href="shoppingCart.php" class="nav-link px-2 text-white">Warenkorb</a></li>
+                    <li class="me-2"><a href="profil.php" class="nav-link px-2 text-white">Profil</a></li>
+                  </ul>
+                <!-- Login/Signout -->
+                  <button type="button" class="btn btn-outline-light me-2">Login</button>
+                  <button type="button" class="btn btn-warning">Sign-up</button>
+                </div>
 
+            </div>
+        </div>
     </header>
 
 
-    <footer class="footer">
+    <footer class="footer p-3 bg-dark text-white">
         <div class="container">
-            <div class="row">
+            <div class="row d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <div class="col">
                   </div>
                   <div class="col-2">
-                    <img src="C:\xampp\htdocs\web\webShop\database\images\pngwing.com (1).png"><button class="footerbutton">Doku</button>
+                    <img src=".../assets/images/git.png"><button class="footerbutton">Doku</button>
                   </div>
                   <div class="col-2">
                     <img src=""><button class="footerbutton">DevTeam</button>
