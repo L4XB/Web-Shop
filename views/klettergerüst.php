c22ede1e1146ac7572c32a3ad458d75793cc9f70
QW_ellCODe <walzquentin1@gmail.com>
2023-11-16 21:53:00 +0100

    Remove unnecessary header and navigation code.

--- views/klettergerüst.php
+++ views/klettergerüst.php
@@ -14,74 +14,11 @@
 </head>
 
 <body>
-<header class="p-3 bg-dark text-white">
-    <nav>  
-    <div class="container">
+    <header>
 
-    <header class="p-3 bg-dark text-white">
-        <div class="container">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                        <use xlink:href="#bootstrap"></use>
-                    </svg>
-                </a>
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-                </ul>
-                <li>
-                    <a href="#" class="nav-link text-secondary">
-                        <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                            <use xlink:href="#home"></use>
-                        </svg>
-                        Home
-                    </a>
-                    </li>
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                    <input type="search" class="form-control form-control-dark" placeholder="Search..."
-                        aria-label="Search" id="s1">
-                </form>
-
-                <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                <use xlink:href="#table"></use>
-                            </svg>
-                            Artikelübersicht
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                <use xlink:href="#grid"></use>
-                            </svg>
-                            Warenkorb
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                <use xlink:href="#people-circle"></use>
-                            </svg>
-                            Profil
-                        </a>
-                    </li>
-                </ul>
-
-                <div class="text-end">
-                    <button type="button" class="btn btn-outline-light me-2">Login</button>
-                    <button type="button" class="btn btn-warning">Sign-up</button>
-                </div>
-            </div>
-    </div>
-    </nav>  
     </header>
+
+
     <footer class="footer">
         <div class="container">
             <div class="row">
@@ -100,8 +37,6 @@
                   </div>
             </div>
     </footer>
-        </div>
-    </header>  
 </body>
 
 </html>
