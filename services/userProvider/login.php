9a824850984088fd0f9ce4642983816b063eec31
Lukas Buck <Lukas.buck@e-mail.de>
2023-11-19 14:21:04 +0100

    check if it is first Login

--- services/userProvider/login.php
+++ services/userProvider/login.php
@@ -1,4 +1,31 @@
 <?php
+
+function isFirstLogin()
+{
+    //Server Connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "webshop";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+    }
+    session_start();
+    $email = $_SESSION['email'];
+    $sql = "SELECT isFirstLogin FROM users WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+    session_destroy();
+    return $user['isFirstLogin'] == 1;
+}
+
+
 include '2fa.php';
 //Server Connection
 $servername = "localhost";
@@ -42,6 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['name'] = $user['vorname'];
         $_SESSION['lastLogIn'] = $user['lastLogIn'];
         $_SESSION['email'] = $username;
+
 
         if (is2FAEnabled()) {
             header('Location: ../../views/check_2fa.php');
