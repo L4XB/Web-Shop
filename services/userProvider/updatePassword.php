159dc3c9562afdb33625856d69a374334f8da7ff
Lukas Buck <Lukas.buck@e-mail.de>
2023-11-19 15:24:40 +0100

    update password

---
+++ services/userProvider/updatePassword.php
@@ -0,0 +1,47 @@
+<?php
+include '2fa.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $servername = "localhost";
+    $username = "root";
+    $dbpassword = "";
+    $dbname = "webshop";
+
+    // Erstellen der Verbindung
+    $conn = new mysqli($servername, $username, $dbpassword, $dbname);
+
+    // Überprüfen der Verbindung
+    if ($conn->connect_error) {
+        die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+    }
+    $newPassword = $_POST['newPassword'];
+    $confirmPassword = $_POST['confirmPassword'];
+
+    if ($newPassword === $confirmPassword) {
+        $hashedPassword = hash('sha256', $newPassword);
+        session_start();
+
+        $sql = "UPDATE users SET passwort = ? WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ss", $hashedPassword, $_SESSION['email']);
+        $stmt->execute();
+
+        $sql = "UPDATE users SET isFirstLogin = false WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $_SESSION['email']);
+        $stmt->execute();
+
+        session_destroy();
+
+        echo "Passwort erfolgreich aktualisiert.";
+        if (is2FAEnabled()) {
+            header('Location: ../../views/check_2fa.php');
+        } else {
+            header('Location: ../../views/homepage.php');
+        }
+    } else {
+        echo "Die Passwörter stimmen nicht überein.";
+    }
+}
+?>
--- views/setNewPassword.php
+++ views/setNewPassword.php
@@ -5,10 +5,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Password</title>
+
+    <!--Style-->
+    <link rel="stylesheet" href="../style/signUp.css">
+    <link rel="stylesheet" href="../style/login.css">
+    <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
 </head>
 
 <body>
+    <div class="container-body">
+        <div id="container-left-side">
+            <div id="container-left-side-items">
+                <form id="signUpForm" method="post" action="">
+                    <h1>Erstellen Sie ein Passwort</h1>
+                    <p>Mit diesem Passwort können Sie sich in Zukunft in Ihr Benutzerprofil bei uns einloggen. </p>
+                    <br>
+                    <div class="form-field">
+                        <label for="email">Passwort:</label>
+                        <input placeholder="Geben Sie ein Passwort ein" style="width: 200%;" type="password"
+                            id="newPassword" name="password" required>
+                    </div>
 
+                    <br>
+                    <div class="form-row">
+                        <div class="form-field">
+                            <label for="password">Passwort wiederholen:</label>
+                            <input placeholder="Bestätigen Sie ihrt Passwort" type="password" id="confirmPassword"
+                                name="passwordSe" style="width: 200%;" required>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+                    <br>
+                    <div class="form-row">
+                        <a id="rejectText" href="login.php">
+                            <p>abbrechen</p>
+                        </a>
+
+                        <button style="width: 50%;" name="submit" type="submit">Weiter</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        <div id="container-right-side">
+            <img height="550px" src="../assets/images/inf-logo.png" alt="">
+        </div>
+    </div>
 </body>
 
 </html>
