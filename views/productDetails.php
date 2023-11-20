d1f200ff263627aef06181f9599a70ed19c821cf
Lukas Buck <Lukas.buck@e-mail.de>
2023-11-20 15:13:36 +0100

    amount buttons

--- views/productDetails.php
+++ views/productDetails.php
@@ -10,7 +10,23 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/productDetails.css">
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            document.getElementById('minus').addEventListener('click', function () {
+                var number = document.getElementById('number');
+                var currentValue = parseInt(number.innerText);
+                if (currentValue > 1) {
+                    number.innerText = currentValue - 1;
+                }
+            });
 
+            document.getElementById('plus').addEventListener('click', function () {
+                var number = document.getElementById('number');
+                var currentValue = parseInt(number.innerText);
+                number.innerText = currentValue + 1;
+            });
+        });
+    </script>
 </head>
 
 <body>
@@ -23,6 +39,14 @@
                 </div>
                 <div id="details-products">
                     <p id="product-name">{Product Name}</p>
+                    <div id="details-products-data">
+
+                    </div>
+                    <div id="counter">
+                        <div id="minus">-</div>
+                        <div id="number">1</div>
+                        <div id="plus">+</div>
+                    </div>
                 </div>
             </div>
         </div>
