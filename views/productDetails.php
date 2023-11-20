1cf63708828794010271de2c6a51b3ccc5ea8f9a
Lukas Buck <Lukas.buck@e-mail.de>
2023-11-20 14:13:46 +0100

    product details page

--- views/productDetails.php
+++ views/productDetails.php
@@ -9,10 +9,21 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/productDetails.css">
+
 </head>
 
 <body>
     <?php include 'klettergerüst.php'; ?>
+    <div id="content-divs">
+        <div id="black-top-box">
+
+        </div>
+        <div id="white-bottom-box">
+
+        </div>
+    </div>
+
 </body>
 
 </html>
