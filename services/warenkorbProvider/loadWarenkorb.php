9f9efe74c6aa302085a9371cbb2bc070c1839afa
Lukas Buck <Lukas.buck@e-mail.de>
2023-11-27 15:12:12 +0100

    jochen fix

--- services/warenkorbProvider/loadWarenkorb.php
+++ services/warenkorbProvider/loadWarenkorb.php
@@ -13,36 +13,37 @@ if ($conn->connect_error) {
 
 }
 session_start();
-$UserID=$_SESSION['UserID'];
-$sql = "SELECT * FROM ShoppingCart
-        WHERE userID=$UserID";
-$result = $conn->query($sql);
+$UserID = $_SESSION['UserID'];
+$stmt = $conn->prepare("SELECT * FROM ShoppingCart WHERE userID = ?");
+$stmt->bind_param("i", $UserID); // "i" steht für "integer"
+$stmt->execute();
+
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-while ($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         //Warenkorb insert
         echo '<div class="row">';
 
-        echo '<div class="col-2"></div>';    
-            
+        echo '<div class="col-2"></div>';
+
         echo '<div class="col-1" style="justify-content: center; align-items: center; display: flex; border-bottom: solid; border-width:thin; border-color: lightgrey;"><button class="delete"><img src="../database/images/trash.png" style="height: 30%;"></button></div>';
 
         echo '<div class="col-1" style="justify-content: right; align-items: center; display: flex; border-bottom: solid; border-width:thin; border-color: lightgrey;"><img src="' . $row['product_image'] . '" alt=""></div>';
-            
-        echo '<div class="col-2" style="border-bottom: solid; border-width:thin; border-color: lightgrey;"><p class="light-text">' . $row['product_name'] . '</p><p class="text">' . $row['product_shortdescription'] . '</p></div>';    
-            
+
+        echo '<div class="col-2" style="border-bottom: solid; border-width:thin; border-color: lightgrey;"><p class="light-text">' . $row['product_name'] . '</p><p class="text">' . $row['product_shortdescription'] . '</p></div>';
+
         echo '<div class="col-2" style="justify-content: center; align-items: center; display: flex; border-bottom: solid; border-width:thin; border-color: lightgrey;"><div id="counter"><div id="minus">-</div><div id="number">1</div><div id="plus">+</div></div></div>';
-            
+
         echo '<div class="col-1" style="justify-content: right; align-items: center; display: flex; border-bottom: solid; border-width:thin; border-color: lightgrey;"><p>' . $row['product_price'] . '</p></div>';
-            
+
         echo '<div class="col-2"></div>';
 
         echo '</div>';
-}}
-else {
+    }
+} else {
     echo "Keine Artikel im Warenkorb.";
 }
 
 // Verbindung schließen
-$conn->close();?>
-    
+$conn->close(); ?>
--- views/setNewPassword.php
+++ views/setNewPassword.php
@@ -10,6 +10,16 @@
     <link rel="stylesheet" href="../style/signUp.css">
     <link rel="stylesheet" href="../style/login.css">
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
+    <script>
+        function showAlert() {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+            })
+        }
+    </script>
 </head>
 
 <body>
