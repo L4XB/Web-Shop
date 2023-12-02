9ab2b3c35ced954d3dcea303ddce097801829500
QW_ellCODe <walzquentin1@gmail.com>
2023-12-02 01:22:27 +0100

    Homepage do over

--- views/homepage.php
+++ views/homepage.php
@@ -73,7 +73,7 @@
                             <div class="carousel-caption text-left text-color">
                                 <h1>DevTeam</h1>
                                 <p>Lerne die Entwickler des FSI Shops kennen!</p>
-                                <p><a class="btn btn-warning" href="devteam.php" role="button">Visit</a></p>
+                                <p><a class="btn btn-dark" href="devteam.php" role="button">Visit</a></p>
                             </div>
                         </div>
                     </div>
@@ -83,17 +83,17 @@
                         <div class="carousel-caption text-color">
                             <h1>Herzlich Willkommen</h1>
                             <p>Hier findest du den besten Merch der Hochschule Reutlingen!</p>
-                            <p><a class="btn btn-warning" href="products.php" role="button">Artikelübersicht</a></p>
+                            <p><a class="btn btn-dark" href="products.php" role="button">Artikelübersicht</a></p>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img class="third-slide" img src="..//assets/images/homepage/ banner.png" alt="Third slide">
+                    <img class="third-slide" img src="..//assets/images/homepage/banner.png" alt="Third slide">
                     <div class="container">
                         <div class="carousel-caption text-right text-color">
                             <h1>Dokumentation</h1>
                             <p>Du interresierst dich für die Entstehung des Webshops?</p>
-                            <p><a class="btn btn-warning" href="doku.php" role="button">check out</a></p>
+                            <p><a class="btn btn-dark" href="doku.php" role="button">check out</a></p>
                         </div>
                     </div>
                 </div>
@@ -116,74 +116,76 @@
             ================================================== -->
             <!-- Wrap the rest of the page in another container to center all the content. -->
 
-            <div class="container marketing">
+        <div class="container marketing">
 
-                <!-- Three columns of text below the carousel -->
-                <div class="row">
-                <div class="col-lg-4">
-                    <img class="rounded-circle" img src="..//assets/images/homepage/fsi_logo.png" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Überschrift</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-lg-4">
-                    <img class="rounded-circle" img src="..//assets/images/homepage/fsi_logo.png" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Überschrift</h2>
-                    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-lg-4">
-                    <img class="rounded-circle" img src="..//assets/images/homepage/fsi_logo.png" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Überschrift</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-                </div><!-- /.col-lg-4 -->
-                </div><!-- /.row -->
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+            <div class="col-lg-4">
+                <img class="rounded-circle" img src="..//assets/images/homepage/fsi_logo.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Überschrift</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+                <img class="rounded-circle" img src="..//assets/images/homepage/fsi_logo.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Überschrift</h2>
+                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+                <img class="rounded-circle" img src="..//assets/images/homepage/fsi_logo.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Überschrift</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
 
 
-                <!-- START THE FEATURETTES -->
+            <!-- START THE FEATURETTES -->
 
-                <hr class="featurette-divider">
+            <hr class="featurette-divider">
 
-                <div class="row featurette">
+            <div class="row featurette">
                 <div class="col-md-7">
-                    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    <h2 class="featurette-heading">Premium Men´s Brushed Kangaroo Pocket Hoodie <span class="text-muted">More then just merch.</span></h2>
+                    <p class="lead">Der Premium Men's Brushed Kangaroo Pocket Hoodie vereint erstklassige Qualität mit stilvollem Design, und hebt sich damit deutlich von herkömmlicher Merchandise-Kleidung ab. Dieser Hoodie verkörpert mehr als nur Merchandise – er ist ein Statement für anspruchsvollen Stil und Komfort.</p>
                 </div>
                 <div class="col-md-5">
-                    <img class="featurette-image img-fluid mx-auto" img src="..//assets/images/homepage/fsi_logo.png" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18c263ad123%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18c263ad123%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.1171875%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                    <img class="featurette-image img-fluid mx-auto" img src="..//assets/images/produkts/hoody/hoody_front_men_fsi-tiger.png" alt="500x500" style="width: auto; height: 500px;"  data-holder-rendered="true">
                 </div>
-                </div>
+            </div>
 
-                <hr class="featurette-divider">
+            <hr class="featurette-divider">
 
-                <div class="row featurette">
+            <div class="row featurette">
                 <div class="col-md-7 order-md-2">
-                    <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    <h2 class="featurette-heading">Women´s non brushed Kangaroo Pocket Hoodie <span class="text-muted">For the Girls</span></h2>
+                    <p class="lead">Der Women's Kangaroo Pocket Hoodie bietet nicht nur bequeme Alltagskleidung, sondern seinen Schnitt auch einen besonderen Ausdruck von Weiblichkeit und Individualität. Dieser Hoodie ist mehr als nur Bekleidung - er ist eine persönliche Aussage für selbstbewusste Frauen mit einem Sinn für Stil.</p>
                 </div>
                 <div class="col-md-5 order-md-1">
-                    <img class="featurette-image img-fluid mx-auto" img src="..//assets/images/homepage/fsi_logo.png" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18c263ad129%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18c263ad129%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.1171875%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                    <img class="featurette-image img-fluid mx-auto" img src="..//assets/images/produkts/hoody/hoody_front_women_fsi-tiger.png" alt="500x500" style="width: auto; height: 500px;" data-holder-rendered="true">
                 </div>
-                </div>
+            </div>
 
-                <hr class="featurette-divider">
+            <hr class="featurette-divider">
 
-                <div class="row featurette">
+            <div class="row featurette">
                 <div class="col-md-7">
-                    <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    <h2 class="featurette-heading">Black FSI Mug <span class="text-muted">Coffee is only the beginning...</span></h2>
+                    <p class="lead">Die schwarze FSI-Tasse ist ein echter alrounder, da sie für weit mehr als nur für Kaffee geeignet ist. Diese Tasse aus bestem Porzelan steht für vielseitigen Genuss und lädt dazu ein, mehr als nur das übliche Getränk daraus zu erleben.</p>
                 </div>
                 <div class="col-md-5">
-                    <img class="featurette-image img-fluid mx-auto" img src="..//assets/images/homepage/fsi_logo.png" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18c263ad12a%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18c263ad12a%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.1171875%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                    <img class="featurette-image img-fluid mx-auto" img src="..//assets/images/produkts/tasse/tasse_fsi.png" alt="500x500" style="width: 500px; height: auto;" data-holder-rendered="true">
                 </div>
-                </div>
+                <br>
+                <br>
+            </div>
 
-                <hr class="featurette-divider">
+            <hr class="featurette-divider">
 
-                <!-- /END THE FEATURETTES -->
-            </div><!-- /.container -->
-        </main>
+            <!-- /END THE FEATURETTES -->
+        </div><!-- /.container -->
+    </main>
     
 
             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
