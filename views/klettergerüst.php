c3c664bfcc72544a5b652ccc3cdb7541c388efac
moechtegernmoench <moenchstalweg@gmail.com>
2023-12-05 21:01:14 +0100

    änderung rückgängig weil gay

--- views/klettergerüst.php
+++ views/klettergerüst.php
@@ -5,21 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Klettergerüst</title>
+  <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
+  <link rel="stylesheet" href="../style/klettergerüst.css">
 
-<!-- Bootstrap core CSS -->
-  <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- CSS -->
-<link rel="stylesheet" href="../style/klettergerüst.css">
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  
-<link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script>
     document.addEventListener('DOMContentLoaded', (event) => {
       var button2 = document.getElementById("button2");
