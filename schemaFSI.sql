ff82141cc9925267328f26c607a97f86e1ade648
Lukas Buck <Lukas.buck@e-mail.de>
2023-12-14 11:37:47 +0100

    schema update

--- schemaFSI.sql
+++ schemaFSI.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 12. Dez 2023 um 14:02
+-- Erstellungszeit: 14. Dez 2023 um 11:37
 -- Server-Version: 10.4.28-MariaDB
 -- PHP-Version: 8.2.4
 
@@ -20,6 +20,45 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `webShopFSI`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `favoritesID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `productID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `favorites`
+--
+
+INSERT INTO `favorites` (`favoritesID`, `userID`, `productID`) VALUES
+(79, 19, 2),
+(82, 19, 1),
+(83, 19, 3),
+(84, 19, 5),
+(98, 19, 9),
+(99, 20, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `history`
+--
+
+CREATE TABLE `history` (
+  `historyID` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `amount` int(11) DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `productID` int(11) DEFAULT NULL,
+  `transactionID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -52,9 +91,91 @@ INSERT INTO `products` (`productID`, `productName`, `price`, `size`, `fit`, `pat
 (8, 'Product8', 89.99, 'XL', 'Slim Fit', 'hoody_front_men_fsi-tiger', 'Description for Product8'),
 (9, 'Product9', 99.99, 'M', 'Loose Fit', 'Tiger-Tasche', 'Description for Product9');
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `shoppingCart`
+--
+
+CREATE TABLE `shoppingCart` (
+  `cartID` int(11) NOT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `productID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `shoppingCart`
+--
+
+INSERT INTO `shoppingCart` (`cartID`, `amount`, `userID`, `productID`) VALUES
+(18, 3, 19, 2),
+(19, 7, 19, 5),
+(20, 10, 19, 7),
+(21, 4, 20, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `transactionID` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `passwort` varchar(255) DEFAULT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(50) DEFAULT NULL,
+  `lastLogin` timestamp NOT NULL DEFAULT current_timestamp(),
+  `screenResolution` varchar(20) DEFAULT NULL,
+  `os` varchar(50) DEFAULT NULL,
+  `twoFASecret` varchar(255) DEFAULT NULL,
+  `use2FA` tinyint(1) DEFAULT NULL,
+  `isFirstLogin` tinyint(1) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`userID`, `email`, `passwort`, `firstName`, `lastName`, `lastLogin`, `screenResolution`, `os`, `twoFASecret`, `use2FA`, `isFirstLogin`, `createdAt`) VALUES
+(19, 'Madeleine.buck@online.de', '6d21b366238e644c392b8c8c37fb37f48279f31f5dc8911fbd4af1fd97cd43fd', 'Madeleine', 'Buck', '2023-12-14 05:31:23', 'unknown', 'unknown', '7II545CLE7CMXAFI', 0, 1, '2023-12-14 05:31:23'),
+(20, 'buck.lukas@icloud.com', '5fcc724749296d0a0398482ba32ddac7120209ea5ae78248a916c0e272b4b7cb', 'Madeleine', 'Buck', '2023-12-14 07:12:50', 'unknown', 'unknown', '233SVSAA4E55QTKB', 0, 1, '2023-12-14 07:12:50'),
+(21, 'lukas.buck@e-mail.de', '50b0112b8496beed2d9eb1e6349e3c16702fc5f5121627da55742c07cf427514', 'Madeleine', 'Buck', '2023-12-14 07:35:17', 'unknown', 'unknown', 'J47VSZY5S4N5AAAT', 0, 1, '2023-12-14 07:35:17'),
+(22, 'buck.lukas@icloud.com', 'cf93b2073db715b0d130ec8baca419d6a081ffd1b26886bf00bab5ec2c9fada1', 'Madeleine', 'Buck', '2023-12-14 07:40:56', 'unknown', 'unknown', '6SFIVAKUKYGUZZ2S', 0, 1, '2023-12-14 07:40:56');
+
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`favoritesID`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `productID` (`productID`);
+
+--
+-- Indizes für die Tabelle `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`historyID`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `productID` (`productID`),
+  ADD KEY `transactionID` (`transactionID`);
 
 --
 -- Indizes für die Tabelle `products`
@@ -63,14 +184,90 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`productID`);
 
 --
+-- Indizes für die Tabelle `shoppingCart`
+--
+ALTER TABLE `shoppingCart`
+  ADD PRIMARY KEY (`cartID`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `productID` (`productID`);
+
+--
+-- Indizes für die Tabelle `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`transactionID`);
+
+--
+-- Indizes für die Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
 -- AUTO_INCREMENT für exportierte Tabellen
 --
+
+--
+-- AUTO_INCREMENT für Tabelle `favorites`
+--
+ALTER TABLE `favorites`
+  MODIFY `favoritesID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+
+--
+-- AUTO_INCREMENT für Tabelle `history`
+--
+ALTER TABLE `history`
+  MODIFY `historyID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT für Tabelle `products`
 --
 ALTER TABLE `products`
   MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT für Tabelle `shoppingCart`
+--
+ALTER TABLE `shoppingCart`
+  MODIFY `cartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT für Tabelle `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `transactionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `favorites`
+--
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
+  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`);
+
+--
+-- Constraints der Tabelle `history`
+--
+ALTER TABLE `history`
+  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
+  ADD CONSTRAINT `history_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`),
+  ADD CONSTRAINT `history_ibfk_3` FOREIGN KEY (`transactionID`) REFERENCES `transactions` (`transactionID`);
+
+--
+-- Constraints der Tabelle `shoppingCart`
+--
+ALTER TABLE `shoppingCart`
+  ADD CONSTRAINT `shoppingcart_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
+  ADD CONSTRAINT `shoppingcart_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
