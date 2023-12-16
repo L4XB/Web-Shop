a522e23e69c2f0295b288d8bfa875a230dda4a6a
Lukas Buck <Lukas.buck@e-mail.de>
2023-12-16 14:38:05 +0100

    2fa deaktivieren

--- services/userProvider/disable2FA.php
+++ services/userProvider/disable2FA.php
@@ -1,6 +1,19 @@
 <?php
-include '2FA.php';
-disable2FA();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webShopFSI";
+session_start();
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+
+$userID = $_SESSION['userId'];
+$sql = "UPDATE users SET use2FA = false WHERE userID = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $userID);
+$stmt->execute();
 header('Location: ../../views/profil.php');
-exit;
 ?>
