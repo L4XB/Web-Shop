0fe21c91f4b1d518ca937764a61df4733204065f
Lukas Buck <Lukas.buck@e-mail.de>
2023-12-16 18:56:11 +0100

    password min 9 characters

--- views/setNewPassword.php
+++ views/setNewPassword.php
@@ -11,7 +11,28 @@
     <link rel="stylesheet" href="../style/login.css">
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
     <script>
+        function validateForm() {
+            // Holen Sie die Passwörter aus den Eingabefeldern
+            var password = document.getElementById('newPassword').value;
+            var confirmPassword = document.getElementById('confirmPassword').value;
 
+            // Überprüfen Sie, ob die Passwörter übereinstimmen
+            if (password !== confirmPassword) {
+                // Wenn die Passwörter nicht übereinstimmen, zeigen Sie eine Fehlermeldung an und verhindern Sie das Absenden des Formulars
+                document.getElementById('passwordError').textContent = 'Passwörter stimmen nicht überein';
+                return false;
+            }
+
+            // Überprüfen Sie, ob die Passwörter mindestens 9 Zeichen lang sind
+            if (password.length < 9) {
+                // Wenn die Passwörter zu kurz sind, zeigen Sie eine Fehlermeldung an und verhindern Sie das Absenden des Formulars
+                document.getElementById('passwordError').textContent = 'Passwort muss mindestens 9 Zeichen lang sein';
+                return false;
+            }
+
+            // Wenn die Validierung erfolgreich war, lassen Sie das Formular absenden
+            return true;
+        }
     </script>
 </head>
 
@@ -19,7 +40,8 @@
     <div class="container-body">
         <div id="container-left-side">
             <div id="container-left-side-items">
-                <form id="signUpForm" method="post" action="../services/userProvider/updatePassword.php">
+                <form id="signUpForm" method="post" action="../services/userProvider/updatePassword.php"
+                    onsubmit="return validateForm()">
                     <h1>Erstellen Sie ein Passwort</h1>
                     <p>Mit diesem Passwort können Sie sich in Zukunft in Ihr Benutzerprofil bei uns einloggen. </p>
                     <br>
@@ -39,7 +61,9 @@
                     </div>
                     <br>
                     <br>
+                    <div id="passwordError" style="color: red;"></div>
                     <br>
+
                     <div class="form-row">
                         <a id="rejectText" href="login.php">
                             <p>abbrechen</p>
