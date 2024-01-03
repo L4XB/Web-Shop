c59d5b9a27b180a747fbd18fe4f5dbcd9f09535b
Lukas Buck <Lukas.buck@e-mail.de>
2024-01-03 18:42:15 +0100

    Fix email retrieval bug and improve password reset UI

--- views/profil.php
+++ views/profil.php
@@ -85,18 +85,23 @@
                         </div>
                         <div class="card-body">
                             <div class="d-flex flex-column align-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-key" viewBox="0 0 16 16">
-                                    <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
-                                    <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                                    class="bi bi-key" viewBox="0 0 16 16">
+                                    <path
+                                        d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5" />
+                                    <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
                                 </svg>
                             </div>
                             <ul class="list-unstyled mt-3 mb-4">
-                                <li>Hier haben Sie die Möglichkeit Ihr Passwort zu Ändern. Beachten Sie, dass das neue Passwort min. 9 Zeichen lang ist, sowie Großbuchstaben, Kleinbuchstaben und Zahlen beinhalten muss.</li>
+                                <li>Hier haben Sie die Möglichkeit Ihr Passwort zu Ändern. Beachten Sie, dass das neue
+                                    Passwort min. 9 Zeichen lang ist, sowie Großbuchstaben, Kleinbuchstaben und Zahlen
+                                    beinhalten muss.</li>
                             </ul>
 
                             <div style="d-flex flex-column align-items-center">
-                                <form action="" method="post" style="margin: 0;">
-                                    <button type="submit" class="btn btn-lg btn-block btn-outline-warning">Passwort ändern</button>
+                                <form action="reset_password/reset_password.php" method="post" style="margin: 0;">
+                                    <button type="submit" class="btn btn-lg btn-block btn-outline-warning">Passwort
+                                        ändern</button>
                                 </form>
                             </div>
                         </div>
@@ -124,7 +129,9 @@
 
                             <div style="d-flex flex-column align-items-center">
                                 <form action="orderHistory.php" method="post" style="margin: 0;">
-                                    <button type="submit" class="btn btn-lg btn-block btn-outline-warning">Bestellhistorie ansehen</button>
+                                    <button type="submit"
+                                        class="btn btn-lg btn-block btn-outline-warning">Bestellhistorie
+                                        ansehen</button>
                                 </form>
                             </div>
                         </div>
