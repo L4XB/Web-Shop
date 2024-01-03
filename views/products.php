3a42388c2e7a4efafba7df737bb38f7571a43e4f
QW_ellCODe <walzquentin1@gmail.com>
2024-01-03 16:14:21 +0100

    Update homepage and products styling

--- views/products.php
+++ views/products.php
@@ -5,10 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products</title>
-    <link rel="stylesheet" href="../style/products.css">
+
+    <!-- favicon -->
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/products.css">
+
     <style>
         .product-card {
             cursor: pointer;
@@ -23,10 +28,16 @@
 
 <body>
     <?php include 'klettergerüst.php'; ?>
-    <br>
-    <div class="container-fluid">
-        <h1 id="headLineTextStyle" style="text-align: left; padding-left: 16%;">Artikelübersicht</h1>
-    <br>
+
+    <div class="container pt-4" style="margin-left: 12%;">
+        <div class="col-lg-6 order-2 order-lg-1">
+            <h1 id="headLineTextStyle">Artikelübersicht</h1>
+        </div>
+        <ul class="breadcrumb undefined">
+            <li class="breadcrumb-item"><a href="homepage.php" class="text-dark">Home</a></li>
+            <li class="breadcrumb-item active">Artikelübersicht</a></li>
+        </ul>
+    </div>
 
     <?php
     include("../services/productProvider/loadAllProducts.php");
