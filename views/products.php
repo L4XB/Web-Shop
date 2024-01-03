93ebf148197eb74299ecf583a2592e26089ef375
Lukas Buck <Lukas.buck@e-mail.de>
2024-01-03 16:19:21 +0100

    Update Bootstrap and add custom CSS to products page

--- views/products.php
+++ views/products.php
@@ -12,7 +12,9 @@ session_start()
     <!-- favicon -->
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/products.css">
     <style>
         .product-card {
             cursor: pointer;
@@ -30,7 +32,7 @@ session_start()
     <br>
     <div class="container-fluid">
         <h1 id="headLineTextStyle" style="text-align: left; padding-left: 16%;">Artikelübersicht</h1>
-    <br>
+        <br>
 
         <?php
         include("../services/productProvider/loadAllProducts.php");
