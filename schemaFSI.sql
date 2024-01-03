72263b8ba139394a37b75d79ccb4e9cbb1876a24
Lukas Buck <Lukas.buck@e-mail.de>
2024-01-03 16:26:39 +0100

    Refactor code to improve performance and readability

--- schemaFSI.sql
+++ schemaFSI.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 18. Dez 2023 um 12:56
+-- Erstellungszeit: 03. Jan 2024 um 16:26
 -- Server-Version: 10.4.28-MariaDB
 -- PHP-Version: 8.2.4
 
@@ -18,8 +18,28 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: =====> `webShopFSI` <=====
+-- Datenbank: `webShopFSI`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `discount`
+--
+
+CREATE TABLE `discount` (
+  `discountID` int(11) NOT NULL,
+  `discountCode` varchar(255) NOT NULL,
+  `discountInPercent` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `discount`
+--
+
+INSERT INTO `discount` (`discountID`, `discountCode`, `discountInPercent`) VALUES
+(3, 'FSI15', 15.00),
+(4, 'QUENTIN20', 20.00);
 
 -- --------------------------------------------------------
 
@@ -38,8 +58,8 @@ CREATE TABLE `favorites` (
 --
 
 INSERT INTO `favorites` (`favoritesID`, `userID`, `productID`) VALUES
-(103, 30, 1),
-(104, 31, 1);
+(114, 49, 2),
+(117, 49, 3);
 
 -- --------------------------------------------------------
 
@@ -61,15 +81,8 @@ CREATE TABLE `history` (
 --
 
 INSERT INTO `history` (`historyID`, `timestamp`, `amount`, `userID`, `productID`, `transactionID`) VALUES
-(39, '2023-12-16 14:52:57', 11, 31, 2, 13),
-(40, '2023-12-16 14:52:57', 4, 31, 7, 13),
-(41, '2023-12-16 14:52:57', 5, 31, 4, 13),
-(42, '2023-12-16 14:52:57', 1, 31, 7, 13),
-(46, '2023-12-16 15:31:50', 4, 31, 2, 14),
-(47, '2023-12-16 16:45:06', 5, 31, 1, 15),
-(48, '2023-12-16 16:45:06', 5, 31, 7, 15),
-(50, '2023-12-16 17:30:13', 5, 31, 2, 16),
-(51, '2023-12-16 17:30:13', 10, 31, 7, 16);
+(112, '2024-01-03 10:49:44', 10, 49, 7, 46),
+(113, '2024-01-03 10:49:44', 3, 49, 2, 46);
 
 -- --------------------------------------------------------
 
@@ -95,14 +108,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`productID`, `productName`, `price`, `size`, `fit`, `pathName`, `description`, `stock`, `detailedDescription`, `descriptionPoints`) VALUES
-(1, 'FSI Beanie (schwarz)', 19.99, 'default', 'Regular', 'FSI_beanie', 'Stilvoller Logo-Beanie', 100, 'Unsere schwarze Logo-Beanie vereint Stil und Komfort. Die hochwertige Stickerei verleiht dem Beanie einen einzigartigen Touch.', 'Hochwertige Stickerei; Bequemer Sitz; Wärmendes Material; Zeitloses Design'),
-(2, 'FSI Coffee-to-go Becher', 12.99, 'default', NULL, 'FSI-Coffee-to-go', 'Stylischer Logo-Kaffeebecher', 30, 'Genießen Sie Ihren Kaffee unterwegs mit unserem Logo Coffee-to-go Becher.', 'Hochwertiges Material; Auslaufsicherer Deckel; Stylisches Design; Nachhaltig/Umweltfreundlich'),
+(1, 'FSI Beanie (schwarz)', 19.99, 'default', 'Regular', 'FSI_beanie', 'Stilvoller Logo-Beanie', 95, 'Unsere schwarze Logo-Beanie vereint Stil und Komfort. Die hochwertige Stickerei verleiht dem Beanie einen einzigartigen Touch.', 'Hochwertige Stickerei; Bequemer Sitz; Wärmendes Material; Zeitloses Design'),
+(2, 'FSI Coffee-to-go Becher', 12.99, 'default', NULL, 'FSI-Coffee-to-go', 'Stylischer Logo-Kaffeebecher', 21, 'Genießen Sie Ihren Kaffee unterwegs mit unserem Logo Coffee-to-go Becher.', 'Hochwertiges Material; Auslaufsicherer Deckel; Stylisches Design; Nachhaltig/Umweltfreundlich'),
 (3, 'FSI Fahne (schwarz)', 24.99, 'default', NULL, 'FSI-Fahne', 'Strapazierfähige Logo-Fahne', 40, 'Zeigen Sie Flagge mit unserer schwarzen Logo-Fahne! Ein Must-have für alle, die Flagge zeigen wollen.', 'Strapazierfähiges Material; Lebendige Farben; Leicht zu befestigen; Vielseitig einsetzbar'),
-(4, 'FSI Umhängetasche', 29.99, 'default', NULL, 'FSI-Tasche', ' Geräumige Logo-Umhängetasche', 120, 'Unsere Logo Baumwoll-Umhängetasche ist der ideale Begleiter für den Alltag.', 'Geräumiges Hauptfach; Lange Schulterbändern; Robuste Baumwolle; Großes aufgedrucktes Logo'),
+(4, 'FSI Umhängetasche', 29.99, 'default', NULL, 'FSI-Tasche', ' Geräumige Logo-Umhängetasche', 117, 'Unsere Logo Baumwoll-Umhängetasche ist der ideale Begleiter für den Alltag.', 'Geräumiges Hauptfach; Lange Schulterbändern; Robuste Baumwolle; Großes aufgedrucktes Logo'),
 (5, 'FSI Tasse', 14.99, 'default', NULL, 'tasse_fsi', 'Elegante Logo-Tasse', 300, 'Genießen Sie Ihre Lieblingsgetränke in unserer Logo Tasse!', 'Hochwertige Keramik; Spülmaschinenfest; Elegantes Design; Perfekt für Heißgetränke'),
 (6, 'FSI Hoodie (unisex)', 39.99, 'S;M;L;XL', 'Regular', 'Hoody_back_fsi', 'Bequemer Logo-Hoodie', 60, 'Unser schwarzer Logo Unisex Hoodie bietet höchsten Tragekomfort dank des weichen Stoffs und des bequemen Schnitts.', 'Weicher Stoff; Bequemer Schnitt; Logo auf dem Rücken; Vielseitig kombinierbar'),
-(7, 'INFormatiger Hoodie  (women)', 59.99, 'S;M;L;XL', 'Oversize fit', 'hoody_front_women_fsi-tiger', 'Stylischer Women-Hoody', 30, 'Unser Heavy-Kapuzenpullover für Frauen in Schwarz mit Iconic INFormatiger auf der Brust überzeugt durch seinen taillierten Schnitt und die hohe Materialqualität.', 'Taillierter Schnitt; Hohe Materialqualität; Iconic INFormatiger Design; Komfortabler Tragekomfort'),
-(8, 'INFormatiger Hoodie  (men)', 59.99, 'S;M;L;XL', 'Oversize fit', 'hoody_front_men_fsi-tiger', 'Stylischer Men-Hoody', 45, 'Unser Heavy-Kapuzenpullover für Männer in Schwarz mit Iconic INFormatiger auf der Brust verleiht dem Hoody einen lässigen Style und macht ihn zum absoluten Blickfang.', 'Moderner Schnitt; Strapazierfähiges Material; Iconic INFormatiger Design; Lässiger Style'),
+(7, 'INFormatiger Kapuzenpullover (women)', 59.99, 'S;M;L;XL', 'Oversize fit', 'hoody_front_women_fsi-tiger', 'Stylischer Women-Hoody', 20, 'Unser Heavy-Kapuzenpullover für Frauen in Schwarz mit Iconic INFormatiger auf der Brust überzeugt durch seinen taillierten Schnitt und die hohe Materialqualität.', 'Taillierter Schnitt; Hohe Materialqualität; Iconic INFormatiger Design; Komfortabler Tragekomfort'),
+(8, 'INFormatiger Kapuzenpullover (men)', 59.99, 'S;M;L', 'Oversize fit', 'hoody_front_men_fsi-tiger', 'Stylischer Men-Hoody', 45, 'Unser Heavy-Kapuzenpullover für Männer in Schwarz mit Iconic INFormatiger auf der Brust verleiht dem Hoody einen lässigen Style und macht ihn zum absoluten Blickfang.', 'Moderner Schnitt; Strapazierfähiges Material; Iconic INFormatiger Design; Lässiger Style'),
 (9, 'INFormatiger Umhängetasche', 29.99, 'default', NULL, 'Tiger-Tasche', 'Praktische INFormatige Tasche', 80, 'Unsere INFormatige Baumwoll-Umhängetasche überzeugt nicht durch lange Schulterbändern. Hergestellt aus robuster Baumwolle ist sie der ideale Begleiter für den Alltag.', 'Praktisches Format; Lange Schulterbändern; Robuste Baumwolle; Großes INFormatiger Design');
 
 -- --------------------------------------------------------
@@ -123,8 +136,11 @@ CREATE TABLE `shoppingCart` (
 --
 
 INSERT INTO `shoppingCart` (`cartID`, `amount`, `userID`, `productID`) VALUES
-(51, 4, 30, 2),
-(85, 5, 37, 1);
+(154, 3, 49, 2),
+(155, 1, 49, 6),
+(156, 3, 49, 8),
+(157, 3, 49, 8),
+(158, 3, 49, 5);
 
 -- --------------------------------------------------------
 
@@ -146,10 +162,7 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`transactionID`, `timestamp`, `userID`, `orderNumber`, `adress`, `paymentMethod`) VALUES
-(13, '2023-12-16 14:52:57', 31, '5054059972', 'Linsenhoferstraße 49, 82392 Beuren', 'PayPal'),
-(14, '2023-12-16 15:31:50', 31, '1381305391', 'Linsenhoferstraße 49, 82392 Beuren', 'PayPal'),
-(15, '2023-12-16 16:45:06', 31, '1547254552', 'Linsenhoferstraße 49, 82392 Beuren', 'PayPal'),
-(16, '2023-12-16 17:30:13', 31, '5156305669', 'Linsenhoferstraße 49, 82392 Beuren', 'PayPal');
+(46, '2024-01-03 10:49:44', 49, '7709564660', 'Linsenhoferstraße 49, 82392 Beuren', 'PayPal');
 
 -- --------------------------------------------------------
 
@@ -171,21 +184,28 @@ CREATE TABLE `users` (
   `isFirstLogin` tinyint(1) DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `is_logged_in` tinyint(1) DEFAULT NULL,
-  `resetCode` VARCHAR(255)
+  `resetCode` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Daten für Tabelle `users`
 --
 
-INSERT INTO `users` (`userID`, `email`, `passwort`, `firstName`, `lastName`, `lastLogin`, `screenResolution`, `os`, `twoFASecret`, `use2FA`, `isFirstLogin`, `createdAt`, `is_logged_in`) VALUES
-(30, 'Madeleine.buck@online.de', '01e872f647c44fb41465904d88770786f66920ffbc405a29a03e13e66df0e8cf', 'Madeleine', 'Buck', '2023-12-16 07:46:52', 'unknown', 'unknown', '3JB3FMXUVCOGTLKD', 1, 1, '2023-12-16 07:46:52', 1),
-(31, 'buck.lukas@icloud.com', '46b47fb88e9bfb31db08a3587a3e89b40ba14b5c1f644270593a987b70f9eda5', 'Madeleine', 'Buck', '2023-12-16 13:35:04', 'unknown', 'unknown', 'AIDWOP4Z2UGAR36Y', 0, 1, '2023-12-16 13:35:04', 1),
-(37, 'lukas.buck@e-mail.de', '90e47ddcd1835ae7b7f0f0c0391372be72660f0c91c3f2be39d8b783e4fc33057cc63c8ef1fb3d4014f1e25eb3848e3b3bf5c8dfc2318d3a8d28010b6c970f80', 'Madeleine', 'Buck', '2023-12-17 14:27:02', '2560x1440', 'Mac OS', 'FCXOGJ2YTM4E3SMH', 0, 1, '2023-12-17 14:27:02', 1);
+INSERT INTO `users` (`userID`, `email`, `passwort`, `firstName`, `lastName`, `lastLogin`, `screenResolution`, `os`, `twoFASecret`, `use2FA`, `isFirstLogin`, `createdAt`, `is_logged_in`, `resetCode`) VALUES
+(30, 'Madeleine.buck@online.de', '01e872f647c44fb41465904d88770786f66920ffbc405a29a03e13e66df0e8cf', 'Madeleine', 'Buck', '2023-12-16 07:46:52', 'unknown', 'unknown', '3JB3FMXUVCOGTLKD', 1, 1, '2023-12-16 07:46:52', 1, NULL),
+(31, 'buck.lukas@icloud.com', '46b47fb88e9bfb31db08a3587a3e89b40ba14b5c1f644270593a987b70f9eda5', 'Madeleine', 'Buck', '2023-12-16 13:35:04', 'unknown', 'unknown', 'AIDWOP4Z2UGAR36Y', 0, 1, '2023-12-16 13:35:04', 1, NULL),
+(49, 'lukas.buck@e-mail.de', 'd72ac459599db16af557a18f0164214b6a9f267c04f88818bf55446ef4fc631a52e9ec76ff04c5ea36d32480a90fd351c69963f4e269774a1de1e2a31e03ed61', 'Lukas', 'Buck', '2024-01-03 15:01:59', '2560x1440', 'MacOS', 'PG5EWNB3FU4XQC6C', 1, 0, '2024-01-03 10:13:31', 1, '744712');
 
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `discount`
+--
+ALTER TABLE `discount`
+  ADD PRIMARY KEY (`discountID`),
+  ADD UNIQUE KEY `discountCode` (`discountCode`);
 
 --
 -- Indizes für die Tabelle `favorites`
@@ -236,16 +256,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `discount`
+--
+ALTER TABLE `discount`
+  MODIFY `discountID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT für Tabelle `favorites`
 --
 ALTER TABLE `favorites`
-  MODIFY `favoritesID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `favoritesID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 
 --
 -- AUTO_INCREMENT für Tabelle `history`
 --
 ALTER TABLE `history`
-  MODIFY `historyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `historyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=115;
 
 --
 -- AUTO_INCREMENT für Tabelle `products`
@@ -257,19 +283,19 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT für Tabelle `shoppingCart`
 --
 ALTER TABLE `shoppingCart`
-  MODIFY `cartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+  MODIFY `cartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=159;
 
 --
 -- AUTO_INCREMENT für Tabelle `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transactionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `transactionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- Constraints der exportierten Tabellen
