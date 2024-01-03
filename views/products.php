3c42a4d887eeea659eb9af98f99a5963bf1649d0
Lukas Buck <Lukas.buck@e-mail.de>
2024-01-03 16:06:10 +0100

    Add session_start() to PHP files

--- views/products.php
+++ views/products.php
@@ -1,3 +1,6 @@
+<?php
+session_start()
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +11,8 @@
     <link rel="stylesheet" href="../style/products.css">
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         .product-card {
             cursor: pointer;
@@ -26,11 +30,12 @@
     <br>
     <div class="container-fluid">
         <h1 id="headLineTextStyle" style="text-align: left; padding-left: 16%;">Artikelübersicht</h1>
-    <br>
+        <br>
 
-    <?php
-    include("../services/productProvider/loadAllProducts.php");
-    ?>
+        <?php
+        include("../services/productProvider/loadAllProducts.php");
+        ?>
     </div>
 </body>
+
 </html>
