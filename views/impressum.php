eb51b5900f87d1880d439787b2004de9c6817b80
QW_ellCODe <walzquentin1@gmail.com>
2024-01-04 20:30:50 +0100

    implemented "impressum.php"

---
+++ views/impressum.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impressum</title>
+
+    <!-- Favicons -->
+    <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
+
+     <!-- bootstrap css -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- custom css -->
+    <link rel="stylesheet" href="../style/spielplatz.css">
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+
+    <!-- header -->
+    <?php include 'header.php'; ?>
+
+    <div class="container pt-4" style="margin-left: 12%;">
+        <div class="col-lg-6 order-2 order-lg-1">
+            <h1>Impressum</h1>
+        </div>
+        <ul class="breadcrumb undefined">
+            <li class="breadcrumb-item"><a href="homepage.php" class="text-dark">Home</a></li>
+            <li class="breadcrumb-item active"><span class="text-dark">Impressum</span></li>
+        </ul>
+    </div>
+        
+    <main class="container">
+        <div class="bg-body-tertiary p-5 rounded">
+            <p class="lead"> Du hast eine Frage, Kritik oder Anregung? Gerne kannst Du dafür unser <a href="kontakt.php" class="text-dark">Kontaktformular</a> verwenden. <br><br>
+                Fachschaft Informatik der Hochschule Reutlingen <br>
+            </p>
+            <br>
+            <h2>Verantwortliche Stelle</h2>
+            <p class="lead">Fachschaft Informatik <br>
+                Fakultät Informatik <br>
+                Hochschule Reutlingen <br>
+                Alteburgstraße 150 <br>
+                72762 Reutlingen <br>
+                <br>
+                <strong>E-Mail:</strong> <a href="mailto: inf.fachschaft@reutlingen-university.de" class="text-dark">inf.fachschaft@reutlingen-university.de</a> <br>
+            </p>
+            <br>
+            <h3>Streitschlichtung</h3>
+            <p class="lead">Online-Streitbeilegung gemäß Art. 14 Abs. 1 ODR-VO: Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit, die Sie unter <a href="https://ec.europa.eu/consumers/odr/" class="text-dark">https://ec.europa.eu/consumers/odr/</a> finden.
+                Wir bemühen uns stets, etwaige Unstimmigkeiten mit dem Kunden im Hinblick auf bestehende Verträge einvernehmlich zu klären. Wir möchten Sie jedoch darauf hinweisen, dass wir zu keiner Teilnahme an einem Verbraucherschlichtungsverfahren (nach VSBG) verpflichtet sind und unseren Kunden die Teilnahme an einem solchen Verfahren leider auch nicht anbieten.
+            </p>
+            <br>
+            <h6>Quelle Codingtools:</h6>
+            <p class="lead"><a href="https://getbootstrap.com/" class="text-dark">https://getbootstrap.com/</a>, <a href="https://sweetalert2.github.io/" class="text-dark">https://sweetalert2.github.io/</a></p>
+        </div>
+    </main>
+</body>
+</html>
