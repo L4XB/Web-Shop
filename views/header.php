bcb6a04e99e9f3828ef1ca02ad626cbedf555fd4
Lukas Buck <Lukas.buck@e-mail.de>
2024-01-04 20:21:51 +0100

    Remove empty line in klettergerüst.php

--- views/header.php
+++ views/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,33 +9,41 @@
     <!-- Favicons -->
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
 
-     <!-- bootstrap css -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- custom css -->
     <link rel="stylesheet" href="../style/header.css">
 
     <!-- bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
     <script>
         document.addEventListener('DOMContentLoaded', (event) => {
-        var button2 = document.getElementById("button2");
-        var badge = document.querySelector(".badge");
+            var button2 = document.getElementById("button2");
+            var badge = document.querySelector(".badge");
 
-        // Verstecke den Info-Kreis, wenn die Seite geladen wird und der Wert 0 ist
-        badge.style.display = badge.textContent > 0 ? "inline" : "none";
+            // Verstecke den Info-Kreis, wenn die Seite geladen wird und der Wert 0 ist
+            badge.style.display = badge.textContent > 0 ? "inline" : "none";
 
-        button2.onclick = function () {
-            var badgeValue = parseInt(badge.textContent, 10);
-            badgeValue++;
-            badge.textContent = badgeValue;
-            badge.style.display = badgeValue > 0 ? "inline" : "none";
-        }
+            button2.onclick = function () {
+                var badgeValue = parseInt(badge.textContent, 10);
+                badgeValue++;
+                badge.textContent = badgeValue;
+                badge.style.display = badgeValue > 0 ? "inline" : "none";
+            }
         });
     </script>
+    <style>
+        .profile-icon {
+            margin-left: 11px;
 
+        }
+    </style>
 </head>
+
 <body>
 
     <?php
@@ -42,31 +51,31 @@
     if (!isset($_SESSION['userId'])) {
 
     } else {
-    $userId = $_SESSION['userId'];
+        $userId = $_SESSION['userId'];
 
-    // Serververbindung
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "webShopFSI";
+        // Serververbindung
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "webShopFSI";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-    }
+        if ($conn->connect_error) {
+            die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+        }
 
-    // SQL-Abfrage, um die Anzahl der Elemente im Warenkorb zu ermitteln
-    $stmt = $conn->prepare("SELECT COUNT(DISTINCT productID) AS count FROM shoppingCart WHERE userID = ?");
-    $stmt->bind_param("i", $userId);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
+        // SQL-Abfrage, um die Anzahl der Elemente im Warenkorb zu ermitteln
+        $stmt = $conn->prepare("SELECT COUNT(DISTINCT productID) AS count FROM shoppingCart WHERE userID = ?");
+        $stmt->bind_param("i", $userId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
 
-    $cartCount = $row['count'];
+        $cartCount = $row['count'];
 
-    $stmt->close();
-    $conn->close();
+        $stmt->close();
+        $conn->close();
     }
     ?>
 
@@ -76,87 +85,96 @@
                 <!-- logo -->
                 <div class="col d-flex justify-content-center">
                     <a class="navbar-brand" href="homepage.php">
-                    <img src="../assets/images/fsi_logo_header.png" alt="FSI Reutlingen" width="169px" height="87px">
+                        <img src="../assets/images/fsi_logo_header.png" alt="FSI Reutlingen" width="169px"
+                            height="87px">
                     </a>
                 </div>
 
                 <!-- search bar -->
                 <div class="col justify-content-start">
-                    <form class="d-flex justify-content-center" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Produktsuche..." aria-label="Search">
-                    <button class="btn btn-outline-warning" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                        height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path
-                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                        </svg></button>
+                    <form class="d-flex justify-content-center" role="search" action="search.php" method="get">
+                        <input class="form-control me-2" name="search" type="search" placeholder="Produktsuche..."
+                            aria-label="Search">
+                        <button class="btn btn-outline-warning" type="submit"><svg xmlns="http://www.w3.org/2000/svg"
+                                width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                <path
+                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                            </svg></button>
                     </form>
                 </div>
 
                 <!-- right alligned nav buttons -->
                 <div class="col d-flex justify-content-center">
-                    <ul class="nav justify-content-end nav-underline" style=" <?php echo isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true ? ' ' : ''; ?>">
+                    <ul class="nav justify-content-end nav-underline"
+                        style=" <?php echo isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true ? ' ' : ''; ?>">
                         <li class="nav-item">
                             <a class="nav-link px-2 text-white" href="products.php">
-                            <div class="d-flex flex-column align-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-shop" viewBox="0 0 16 16">
-                                <path
-                                    d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z" />
-                                </svg>
-                            </div>
-                            <div>Artikelübersicht</div>
+                                <div class="d-flex flex-column align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                        class="bi bi-shop" viewBox="0 0 16 16">
+                                        <path
+                                            d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z" />
+                                    </svg>
+                                </div>
+                                <div>Artikelübersicht</div>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link px-2 text-white" href="favorits.php">
-                            <div class="d-flex flex-column align-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-heart" viewBox="0 0 16 16">
-                                <path
-                                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-                                </svg>
-                            </div>
-                            <div>Favoriten</div>
+                                <div class="d-flex flex-column align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                        class="bi bi-heart" viewBox="0 0 16 16">
+                                        <path
+                                            d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                                    </svg>
+                                </div>
+                                <div>Favoriten</div>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link px-2 text-white " href="warenkorb.php">
-                            <div class="d-flex flex-column align-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-cart3" viewBox="0 0 16 16">
-                                <path
-                                    d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-                                </svg>
-                            </div>
-                            <div>Warenkorb</div>
+                                <div class="d-flex flex-column align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                        class="bi bi-cart3" viewBox="0 0 16 16">
+                                        <path
+                                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                                    </svg>
+                                </div>
+                                <div>Warenkorb</div>
                             </a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link px-2 text-white dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                            aria-expanded="false">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="bi bi-person-circle" viewBox="0 0 16 16">
-                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                                <path fill-rule="evenodd"
-                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                            </svg>
-                            <br>Profil
-                            </a>
-                            <ul class="dropdown-menu">
-                            <div class="d-flex flex-column align-items-center">
-                                <li><button class="btn btn-sm btn-outline-light mb-2" type="button"> Login </button></li>
-                                <li><button class="btn btn-sm btn-warning" type="button">Sign-up</button></li>
-                                <!--
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Platzhalter</a></li>
-                                -->
-                            </div>
-                            </ul>
-                        </li>
+                        <div style="margin-top:-4px">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link px-2 text-white dropdown-toggle" data-bs-toggle="dropdown" href="#"
+                                    role="button" aria-expanded="false">
+
+                                    <svg class="profile-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                        fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                        <path fill-rule="evenodd"
+                                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                                    </svg>
+
+                                    <br>Profil
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <div class="d-flex flex-column align-items-center">
+                                        <li><button class="btn btn-sm btn-outline-light mb-2" type="button"> Login
+                                            </button></li>
+                                        <li><button class="btn btn-sm btn-warning" type="button">Sign-up</button></li>
+                                        <!--
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Platzhalter</a></li>
+                      -->
+                                    </div>
+                                </ul>
+                            </li>
+                        </div>
                     </ul>
                 </div>
             </div>
         </nav>
     </header>
 </body>
+
 </html>
