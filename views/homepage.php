330f3db81f2691920a4dea86598d5702cdaf301f
Lukas Buck <Lukas.buck@e-mail.de>
2024-01-06 14:32:10 +0100

    Remove unused favicon links

--- views/homepage.php
+++ views/homepage.php
@@ -10,14 +10,6 @@ session_start();
 
     <!-- Favicons -->
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
-    <!-- 
-    <link rel="apple-touch-icon" sizes="180x180" href="../assets/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../assets/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/icons/favicon-16x16.png">
-    <link rel="manifest" href="../assets/icons/site.webmanifest">
-    <link rel="mask-icon" href="../assets/icons/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#212529">
-    <meta name="theme-color" content="#212529"> -->
 
     <title>Homepage</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
