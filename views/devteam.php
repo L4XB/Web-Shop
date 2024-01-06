97236cb48c8d802c66bb3e34ebdd3fa5681a43a4
Lukas Buck <Lukas.buck@e-mail.de>
2024-01-06 13:50:02 +0100

    Update Bootstrap version and fix styling issues

--- views/devteam.php
+++ views/devteam.php
@@ -9,18 +9,22 @@
     <!-- Favicons -->
     <link rel="icon" type="image/x-icon" href="../assets/icons/favicon-192x192.ico">
 
-     <!-- bootstrap css -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- custom css -->
     <link rel="stylesheet" href="../style/#.css">
 
     <!-- bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
     <style>
         body {
             overflow-y: scroll;
+            height: 100%;
+            margin: 0;
         }
 
         .card-img-wrapper {
@@ -44,6 +48,7 @@
     </style>
 
 </head>
+
 <body>
 
     <!-- header -->
@@ -115,7 +120,6 @@
             </div>
         </div>
     </main>
-
     <!-- footer -->
     <?php include 'footer.php'; ?>
 
