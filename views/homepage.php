fef6abe7e382762746d4d4029ab191eeee3e0deb
Lukas Buck <Lukas.buck@e-mail.de>
2024-01-08 11:42:11 +0100

    Fix session_start() duplication in PHP files

--- views/homepage.php
+++ views/homepage.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $shwoAlert = false;
 
 if (isset($_SESSION['previous_page'])) {
--- views/warenkorb.php
+++ views/warenkorb.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 
 // Überprüfen, ob der Benutzer eingeloggt ist
